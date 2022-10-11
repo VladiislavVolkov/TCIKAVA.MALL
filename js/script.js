9bcc206e8dfeeed1cf864ef85766149612ff7d38
VladiislavVolkov <kali.laskaa@gmail.com>
2022-10-11 10:33:07 +0300

    ver 1.3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,2 @@
-$("header").load("../pages/header.html", () => { console.log("header load for all") });
-$("footer").load("../pages/footer.html", () => { console.log("footer load for all") });
-$("header").load("pages/header.html", () => { console.log("header load for index.html") });
-$("footer").load("pages/footer.html", () => { console.log("footer load for index.html") });
+$("header").load("/pages/header.html", () => { console.log("header load for all") });
+$("footer").load("/pages/footer.html", () => { console.log("footer load for all") });
