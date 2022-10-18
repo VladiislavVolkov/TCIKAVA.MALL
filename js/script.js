6721f16a3cb4cd3ac35e1c4fd2546baee9a483b1
VladiislavVolkov <kali.laskaa@gmail.com>
2022-10-18 14:56:20 +0300

    add preloader

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
+$(".preloader").load("preloader.html", () => { console.log("preloader load") });
 $("header").load("header.html", () => { console.log("header load") });
 $("footer").load("footer.html", () => { console.log("footer load") });
