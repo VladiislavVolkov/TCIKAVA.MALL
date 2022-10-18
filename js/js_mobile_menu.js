f10c0c5d2e000353402513398d8d66407f0d7dea
VladiislavVolkov <kali.laskaa@gmail.com>
2022-10-18 22:21:51 +0300

    new animation for mobile_menu

--- js/js_mobile_menu.js
+++ js/js_mobile_menu.js
@@ -1,7 +1,25 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
+let numberOfClick = 0;
 
 menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
+	if ( numberOfClick % 2 == 0 ) {
+		menuBtn.classList.toggle('active');
+		menu.classList.toggle('active');
+	} else {	
+		menuBtn.classList.remove('active');
+		menu.classList.remove('active');	
+		menu.classList.toggle('active2');
+		setTimeout(()=>{
+			menu.classList.remove('active2');
+		},700);
+	}
+	numberOfClick++;
 })
+
+// menuBtn.addEventListener('click', function(){
+// 	menuBtn.classList.toggle('active');
+// 	menu.classList.toggle('active');
+// })
+
+
