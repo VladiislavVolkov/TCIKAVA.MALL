ac17a02d35c773f4fc3da2cc7e54477687c8e4d3
VladiislavVolkov <kali.laskaa@gmail.com>
2022-10-18 14:10:00 +0300

    new mobile_menu

--- js/js_mobile_menu.js
+++ js/js_mobile_menu.js
@@ -1,16 +1,24 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
 
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+//     let dropdowns = document.getElementsByClassName("dropdown-content");
+//     let i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       let openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
